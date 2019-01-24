93e0f3bf8cf201abf7ee0ada52c799782e2628c9
Dani <daniel@essedi.es>
2019-01-24 10:33:36 +0100

    perform default .env

--- init.sh
+++ init.sh
@@ -21,20 +21,20 @@ echo "Setting env data";
 user=$1;
 passwd="db-"$1;
 client_id=$(mysql -u${user} -p${passwd} ${1} -se 'Select CONCAT(id, "_", random_id) as client_id FROM client LIMIT 1;');
-echo "HERE ${client_id}";
 sed -i.bak 's\'CLIENT_ID=^'\'CLIENT_ID=${client_id}'\' .env.local;
 client_secret=$(mysql -u${user} -p${passwd} ${1} -se 'Select secret FROM client LIMIT 1;');
 sed -i.bak 's\'CLIENT_SECRET=^'\'CLIENT_SECRET=${client_secret}'\' .env.local;
-echo "Enter the base url for this proyect";
-read base_url;
-sed -i.bak 's\'BASE_URL=^'\'BASE_URL=${base_url}'\' .env.local;
-echo "Enter the mailer url";
-read mailer_url;
-sed -i.bak 's\'MAILER_URL=^'\'MAILER_URL=${mailer_url}'\' .env.local;
+read -p "Enter the base url for this proyect [http://${1}.local]" base_url;
+sed -i.bak 's\'BASE_URL=^'\'BASE_URL=${base_url:-http://${1}.local}'\' .env.local;
+read -p "Enter the mailer url [no-reply@${1}.com]" mailer_url;
+sed -i.bak 's\'MAILER_URL=^'\'MAILER_URL=${mailer_url:-no-reply@${1}.com}'\' .env.local;
 
 php bin/console fos:user:create  --super-admin admin admin@$1.local admin-$1
 
 php bin/console assets:install --symlink
 rm .env.local.bak;
+rm init.sh;
+rm db.sh;
+php bin/console cachel:clear;
 echo "Now you can edit the file .env.local to edit variables";
 echo "The backend user is admin with as admin-${1} password";
